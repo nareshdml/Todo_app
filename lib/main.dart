b1b02f52bdeb872aba22b72c43c18afcce006764
Naresh Dhimal <dreamnarus99@gmail.com>
2023-06-12 16:26:41 +0545

    creating textFormField

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "TodoApp",
+      home: TodoApp(),
+    ));
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({super.key});
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Todo App"),
+      ),
+      body: ListView(
+        children: [
+          TextFormField(),
+        ],
+      ),
+    );
+  }
+}
